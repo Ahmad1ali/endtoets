de52a29b00c95635aeb451f0fbc68772d3f414dd
Ahmad1ali <92579960+Ahmad1ali@users.noreply.github.com>
2023-06-22 20:55:33 +0200

    update &delete

--- endtoets/src/Controller/AdminController.php
+++ endtoets/src/Controller/AdminController.php
@@ -3,23 +3,65 @@
 namespace App\Controller;
 
 use App\Entity\Dier;
+use App\Form\NewDierType;
 use App\Repository\DierRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ManagerRegistry;
 use http\Client\Curl\User;
-use Symfony\Bridge\Doctrine\ManagerRegistry;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AdminController extends AbstractController
 {
     #[Route('/admin', name: 'app_admin')]
-    public function index( ManagerRegistry $doctrine, int $id ): Response
+    public function index( ManagerRegistry $doctrine ): Response
     {
-//        $showAdminAnimal = $doctrine->getRepository(Dier::class)->find($id);
+        $name = $this->getUser();
+        $showAdminAnimal = $doctrine->getRepository(Dier::class)->findBy(['user'=>$name]);
 
         return $this->render('admin/index.html.twig', [
-//            'show'=>$showAdminAnimal
+            'show'=>$showAdminAnimal
 
         ]);
     }
+    #[Route('/adddier', name: 'insert')]
+    public function insert (Request $request , EntityManagerInterface $entityManager): Response
+    {
+        $add = new Dier();
+        $form = $this->createForm(NewDierType::class, $add);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $add = $form->getData();
+            $entityManager->persist($add);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_admin');
+        }
+
+        return $this->renderForm('admin/adddier.html.twig', [
+            'form' => $form,
+        ]);
+
+    }
+    #[Route('/deletedier/{id}', name: 'delete')]
+    public function delete(EntityManagerInterface $entityManager, int $id): Response
+    {
+
+        ;
+        $delete = $entityManager->getRepository(Dier::class)->find($id);
+        if (!$delete) {
+            throw $this->createNotFoundException(
+                'No product found for id ' . $id
+            );
+        }
+        $entityManager->remove($delete);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_admin');
+
+    }
+
 }
